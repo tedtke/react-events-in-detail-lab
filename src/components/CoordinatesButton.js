7d2f2ce516936f18ecf14bbea8482831544a56c0
Teodoro Garcia <tedtke514@gmail.com>
2021-03-18 16:02:57 -0500

    commit tree 5cf6000709e7c750138beabc51943b0e16abab6b

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+class CodeCoordinatesButton extends Component {
+  handleMouseMove = (event) => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleMouseMove}> 
+        Here
+      </button>
+    );
+  }
+}
+export default CodeCoordinatesButton
